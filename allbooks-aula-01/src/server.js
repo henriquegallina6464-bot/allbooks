d5dd796ad297c8bcfaef7f628bcb8ca91d665a79
henrique <henriquegallina6464@gmail.com>
2026-02-18 20:05:34 -0300

    altera o server

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -20,6 +20,6 @@ server.use(router) //rotas na prota 3000
 server.listen(8000, () => {
 
   console.log("Boas vindas ao AllBooks")
-  console.log("Acesse essa API em http://localhost:8000")
+  console.log("API disponivel em http://localhost:8000")
 
 })
